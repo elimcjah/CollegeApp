74702e8a4d202aab74dad3677b686d0095fbb6ea
Elijah McClendon <elijah@fungry.com>
2019-03-14 10:26:44 -0600

    Base URI returns package.JSON version number from the backEnd so frontEnd can know when changes propogate

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const packageJSON = require('../package.json');
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    return res.send(`College App Version ${packageJSON.version}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);

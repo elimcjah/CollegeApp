23f0ff432e8d4abeab01b57492bf02c1f6c1e91c
Elijah McClendon <elijah@fungry.com>
2019-03-14 10:44:44 -0600

    GET /schools returns all colleges not 'deleted'

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const packageJSON = require('../package.json');
-
+const { prisma } = require('../prisma/generated/prisma-client');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     return res.send(`College App Version ${packageJSON.version}`);
+});
+
+app.get('/schools', async (req, res) => {
+    try{
+        const allColleges = await prisma.colleges({
+            where: {
+                deleted: false
+            }
+        });
+        return res.send(allColleges);
+    } catch(e) {
+        throw new Error(e);
+    }
 });
 
 app.listen(PORT, () => {
